2ae5ff6c9f68e04a2be333774f16f7efafdc7ebf
Isha Aggarwal <95220687+ishaggarwal13@users.noreply.github.com>
2022-11-06 18:56:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+<<<<<<< Updated upstream
 print(‘Hello world :D’)
+=======
+<<<<<<< HEAD
+print(“Hello Isha :D”)
+=======
+print("Hello world :D")
+>>>>>>> 2a075308bb12fbe9bcef430816f04510dcd01d59
+>>>>>>> Stashed changes
